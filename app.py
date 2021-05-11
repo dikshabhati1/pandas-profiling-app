a70f7a2c330b19356f8d9299760e951760bac5f5
Diksha Bhati <70757239+dikshabhati1@users.noreply.github.com>
2021-05-11 08:11:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,10 +28,23 @@ if uploaded_file is not None:
     st.header('**Pandas Profiling Report**')
     st_profile_report(pr)
     
+    
+    l = []
+    for column in columns_list:
+        data = df[df[column].isna()]
+        l.append(data['ds_instance_id'].value_counts().to_dict())
+    
+    null_rate_df = pd.DataFrame(columns=['Column','Count','null_rate'])
+    null_rate_df['Column'] = [i for i in df.columns]
+    null_rate_df['Count'] = [j for j in df.isnull().sum()]
+    null_rates = df.isnull().sum()/df.shape[0]*100
+    null_rate_df['null_rate'] = [k for k in null_rates]
+    null_rate_df['ds_id_null'] = [l[i] for i in range(len(null_rate_df))]
+    
     def filedownload(df):
         csv = df.to_csv(index=False)
         b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
         href = f'<a href="data:file/csv;base64,{b64}" download="data.csv">Download CSV File</a>'
         return href
 
-    st.markdown(filedownload(df), unsafe_allow_html=True)
+    st.markdown(filedownload(null_rate_df), unsafe_allow_html=True)
