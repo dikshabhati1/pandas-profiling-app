0b1c1cc5c23d275c4561cdfced036433e2c600b0
Diksha Bhati <70757239+dikshabhati1@users.noreply.github.com>
2021-05-11 08:02:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ if uploaded_file is not None:
     def filedownload(df):
         csv = df.to_csv(index=False)
         b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
-        href = f'<a href="data:file/csv;base64,{b64}" download="playerstats.csv">Download CSV File</a>'
+        href = f'<a href="data:file/csv;base64,{b64}" download="data.csv">Download CSV File</a>'
         return href
 
-    st.markdown(filedownload(df_selected_team), unsafe_allow_html=True)
+    st.markdown(filedownload(df), unsafe_allow_html=True)
